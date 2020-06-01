8612381bec9affe943f98ed030a9edc90312cc24
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-06-01 15:31:30 +0100

    Add default lifecycle block back in

--- main.tf
+++ main.tf
@@ -98,7 +98,62 @@ resource "aws_s3_bucket" "this" {
   }
 
   #
-  # Lifecycle Rules
+  # Default Lifecycle Rules
+  #
+  dynamic "lifecycle_rule" {
+    for_each = var.default_lifecycle_rule_enabled == true ? local.default_lifecycle_rule : []
+
+    content {
+      id                                     = lookup(local.default_lifecycle_rule.value, "id", null)
+      abort_incomplete_multipart_upload_days = lookup(local.default_lifecycle_rule.value, "abort_incomplete_multipart_upload_days", null)
+      enabled                                = local.default_lifecycle_rule.value.enabled
+
+      # Max 1 block - expiration
+      dynamic "expiration" {
+        for_each = length(keys(lookup(local.default_lifecycle_rule.value, "expiration", {}))) == 0 ? [] : [lookup(local.default_lifecycle_rule.value, "expiration", {})]
+
+        content {
+          date                         = lookup(expiration.value, "date", null)
+          days                         = lookup(expiration.value, "days", null)
+          expired_object_delete_marker = lookup(expiration.value, "expired_object_delete_marker", null)
+        }
+      }
+
+      # Several blocks - transition
+      dynamic "transition" {
+        for_each = lookup(local.default_lifecycle_rule.value, "transition", [])
+
+        content {
+          date          = lookup(transition.value, "date", null)
+          days          = lookup(transition.value, "days", null)
+          storage_class = transition.value.storage_class
+        }
+      }
+
+      # Max 1 block - noncurrent_version_expiration
+      dynamic "noncurrent_version_expiration" {
+        for_each = length(keys(lookup(local.default_lifecycle_rule.value, "noncurrent_version_expiration", {}))) == 0 ? [] : [lookup(local.default_lifecycle_rule.value, "noncurrent_version_expiration", {})]
+
+        content {
+          days = lookup(noncurrent_version_expiration.value, "days", null)
+        }
+      }
+
+      # Several blocks - noncurrent_version_transition
+      dynamic "noncurrent_version_transition" {
+        for_each = lookup(local.default_lifecycle_rule.value, "noncurrent_version_transition", [])
+
+        content {
+          days          = lookup(noncurrent_version_transition.value, "days", null)
+          storage_class = noncurrent_version_transition.value.storage_class
+        }
+      }
+    }
+  }
+
+
+  #
+  # Custom Lifecycle Rules
   #
   dynamic "lifecycle_rule" {
     for_each = var.default_lifecycle_rule_enabled == false ? var.lifecycle_rule : []
