dc454975eb687600466b6acb106d09cbaf68b8ce
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-11-04 10:08:16 +0000

    Add depends_on

--- main.tf
+++ main.tf
@@ -316,4 +316,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_policy     = var.block_public_policy
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
+
+  depends_on = [aws_s3_bucket_policy.this]
+
 }
