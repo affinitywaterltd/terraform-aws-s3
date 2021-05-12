583efd30075dc79f6f60b90702bb93141f30b8a7
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-05-12 16:05:03 +0100

    Add S3 object wilcard

--- output.tf
+++ output.tf
@@ -8,6 +8,10 @@ output "arn" {
   value       = element(concat(aws_s3_bucket.this.*.arn, list("")), 0)
 }
 
+output "arn_object_wildcard" {
+  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname/*."
+  value       = element(concat("${aws_s3_bucket.this.*.arn}/*", list("")), 0)
+}
 
 output "bucket_domain_name" {
   description = "The bucket domain name. Will be of format bucketname.s3.amazonaws.com."
